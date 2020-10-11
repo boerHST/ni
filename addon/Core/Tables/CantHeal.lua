0bc9dee58e60d57dd534aea6db00d4414744df6b
DarhangeR <civayan@gmail.com>
2020-10-11 16:54:14 +0300

    Update CantHeal.lua

--- addon/Core/Tables/CantHeal.lua
+++ addon/Core/Tables/CantHeal.lua
@@ -1,6 +1,9 @@
 ni.tables.cantheal = {
-	[30843] = "Enfeeble",
-	[41292] = "Aura of Suffering",
-	[45996] = "Darkness",
-	[55593] = "Necrotic Aura"
+    [30843] = "Enfeeble",
+    [41292] = "Aura of Suffering",
+    [45996] = "Darkness",
+    [55593] = "Necrotic Aura"
+}
+ni.tables.notneedheal = {
+    [27827] = "Spirit of Redemption"
 }
